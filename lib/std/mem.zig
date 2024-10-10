bc6edffed58f50cca9668c9f79e30a81d8b18903
YANG Xudong <yangxudong@ymatrix.cn>
2024-10-10 15:43:20 +0800

    use 4k page size

--- lib/std/mem.zig
+++ lib/std/mem.zig
@@ -19,11 +19,11 @@ pub const page_size = switch (builtin.cpu.arch) {
     .sparc64 => 8 * 1024,
     .loongarch32, .loongarch64 => switch (builtin.os.tag) {
         // Linux default KConfig value is 16KiB
-        .linux => 16 * 1024,
+        .linux => 4 * 1024,
         // FIXME:
         // There is no other OS supported yet. Use the same value
         // as Linux for now.
-        else => 16 * 1024,
+        else => 4 * 1024,
     },
     else => 4 * 1024,
 };
