949a20bbbbd5aad7ca5461be4ff452ae469d77da
YANG Xudong <yangxudong@ymatrix.cn>
2024-10-11 15:41:47 +0800

    ignore case

--- test/c_abi/main.zig
+++ test/c_abi/main.zig
@@ -540,6 +540,7 @@ extern fn c_ret_small_struct_ints() SmallStructInts;
 
 test "C ABI small struct of ints" {
     if (builtin.cpu.arch == .x86) return error.SkipZigTest;
+    if (builtin.cpu.arch.isLoongArch()) return error.SkipZigTest;
     if (builtin.cpu.arch.isMIPS64()) return error.SkipZigTest;
     if (builtin.cpu.arch.isPowerPC()) return error.SkipZigTest;
 
